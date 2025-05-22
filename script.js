bbbecb272860305109a9ca86fe844e228e378e23
Higorreis <higorreis17@gmail.com>
2025-05-22 14:30:45 -0300

    Atualizando JS

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ const number = document.querySelector("#number")
 
 button.addEventListener('click', () => {
     try {
-        const min = Math.ceil(parseInt(inputOne.value)) 
+        const min = Math.ceil(parseInt(inputOne.value))
         const max = Math.floor(parseInt(inputTwo.value))
-        number.innerHTML = Math.floor(Math.random() * (max - min + 1)) + min
+        if (min >= max) {
+            number.innerHTML = `X`
+        } else {
+            number.innerHTML = Math.floor(Math.random() * (max - min + 1)) + min
+        }
     } catch (error) {
         console.error(error)
     }
-    
+
 })
